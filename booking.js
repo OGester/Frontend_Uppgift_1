03c1ff09aae61351da563fe4310e5ac31021ff36
Oskar Gester <gester.oskar@gmail.com>
2024-05-28 09:30:44 +0200

    saved seats to local storage, and shows on load at confirmation page

--- booking.js
+++ booking.js
@@ -1,17 +1,18 @@
 const theaterBox = document.querySelector(".theater-box");
 const seats = document.querySelectorAll(".seat:not(.seat-label reserved");
-/* const selectedSeats = document.querySelector(".row .seat: checked");
- */
 
+//function that checks selected seats picks out the id of each one and saves to local storage.
 function selectedSeats() {
   const clickedSeats = document.querySelectorAll(".row .seat:checked");
 
   const arrayOfSeats = Array.from(clickedSeats);
 
   const seatsId = arrayOfSeats.map((seat) => seat.getAttribute("id"));
-  console.log(seatsId);
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsId));
 }
 
+//eventlistener that picks up wich seats are selected and that they are not allready booked.
 theaterBox.addEventListener("click", (e) => {
   if (
     e.target.classList.contains("seat") &&
@@ -19,5 +20,35 @@ theaterBox.addEventListener("click", (e) => {
   ) {
     e.target.classList.toggle("seat-label.available");
   }
+
   selectedSeats();
 });
+
+//redirects to buyticket page for confirmation
+function bookTicket() {
+  window.location.href = "buyTickets.html";
+}
+
+//function that runs when buyticket.html loads and retrieves the reserved seats from local storage
+function getBookedSeats() {
+  if (!selectedSeats.length) {
+    alert("hej");
+  }
+  document.getElementById("selected-seats").innerHTML = JSON.parse(
+    localStorage.selectedSeats
+  );
+}
+
+/* let confirmation = document.getElementById("confirmation");
+let name = document.getElementById("fullName");
+const message = "Thank you for booking";
+
+confirmation.addEventListener("click", function () {
+  alert(name + message);
+}); */
+
+function confirmationAlert() {
+  alert("Your booking is now done! please check yor E-mail");
+  localStorage.clear();
+  location.reload();
+}
